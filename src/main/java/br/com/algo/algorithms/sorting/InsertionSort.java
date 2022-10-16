9da61d5cc3c9ba942e17a1ea03380515f228802e
Jonathan Cabral <dev.jonathancabral@gmail.com>
2022-10-16 13:56:53 -0300

    Implementando algoritimo de busca insertion sort

---
+++ src/main/java/br/com/algo/algorithms/sorting/InsertionSort.java
@@ -0,0 +1,33 @@
+package br.com.algo.algorithms.sorting;
+
+/**
+ * @Description Selection Sort is an in-place comparison-based sorting algorithm, that places an unsorted element at
+ * a suitable place in each iteration
+ * @time-complexity O(N^2)
+ * @see " https://visualgo.net/en/sorting "
+ */
+public class InsertionSort {
+    public static int[] sort(int[] arr) {
+
+        for(int i = 1; i < arr.length; i++) {
+            var atual = arr[i];
+            order(arr, i);
+        }
+
+        return arr;
+    }
+
+    private static int[] order(int[] arr, int start) {
+        var position = start;
+        while(position != 0 && arr[position] < arr[position-1]) {
+            var atual = arr[position];
+            var anterior = arr[position-1];
+
+            arr[position] = anterior;
+            arr[position-1] = atual;
+
+            position--;
+        }
+        return arr;
+    }
+}
---
+++ src/test/java/br/com/algo/algorithms/sorting/InsertionSortTest.java
@@ -0,0 +1,53 @@
+package br.com.algo.algorithms.sorting;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InsertionSortTest {
+    @Test
+    public void dummyTest() {
+        InsertionSort.sort(new int[] {12,5,3,4,14,9});
+    }
+
+    @Test
+    @DisplayName("Ordena lista com 1 registro")
+    public void test1() {
+        int[] arr = {3};
+        int[] result = InsertionSort.sort(arr);
+        assertArrayEquals(arr, result);
+    }
+
+    @Test
+    @DisplayName("Ordena lista com varios registros")
+    public void test2() {
+        int[] expected = {0,1,2,3,4,5,6,7,8,9};
+        int[] result = InsertionSort.sort(new int[] {8,3,5,6,7,0,9,4,1,2});
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("Ordena lista ja ordenada")
+    public void test3() {
+        int[] expected = {0,1,2,3,4,5,6,7,8,9};
+        int[] result = InsertionSort.sort(expected);
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("Ordena lista com todos valores iguais")
+    public void test4() {
+        int[] expected = {0,0,0,0,0,0,0,0,0,0,0,0};
+        int[] result = InsertionSort.sort(expected);
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("Ordena lista ordenada decrescente")
+    public void test5() {
+        int[] expected = {0, 1,2,3,4,5,6,7,8,9};
+        int[] result = InsertionSort.sort(new int[] {9,8,7,6,5,4,3,2,1,0});
+        assertArrayEquals(expected, result);
+    }
+}
