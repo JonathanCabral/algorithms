23d83f75b380472caaf185717f705175926f27e6
Jonathan Cabral <dev.jonathancabral@gmail.com>
2022-10-28 16:24:55 -0300

    Implementação do algoritimo para quicksort, com a escolha do pivot de forma randomica ou não.

--- src/main/java/br/com/algo/algorithms/sorting/QuickSort.java
+++ src/main/java/br/com/algo/algorithms/sorting/QuickSort.java
@@ -2,57 +2,76 @@ package br.com.algo.algorithms.sorting;
 
 import java.util.Random;
 
+/**
+ * Quick sort é um algorithm de ordenação avançado.
+ * Best Case - Todos os items estão foram de ordem e misturados
+ * Worst Case - Todos os items já estão ordenados
+ * Tip - Do not use quicksort with your list already sorted, or try to shuffle it before.
+ *
+ * @TimeComplexity - n(log(n))
+ * @TimeComplexity - n(n^2)
+ */
 public class QuickSort {
 
-    public static int[] sort(int[] array) {
-        quickSort(array, 0, array.length -1);
+    public static boolean isRandomPivot = false;
+
+    public static int[] sort(int[] array, boolean isRandomPivot) {
+        QuickSort.isRandomPivot = isRandomPivot;
+        quickSort(array, 0, array.length - 1);
         return array;
     }
 
     private static void quickSort(int[] array, int lowIndex, int highIndex) {
         //base case
-        if(lowIndex >= highIndex) return;
+        if (lowIndex >= highIndex) return;
 
-        int pivotIndex = new Random().nextInt(highIndex - lowIndex) + lowIndex;
-        int pivot = array[pivotIndex];
-        swap(array, pivotIndex, highIndex);
+        int pivot = getPivot(array, lowIndex, highIndex);
 
         var leftPointer = partition(array, lowIndex, highIndex, pivot);
 
-        quickSort(array, lowIndex, leftPointer-1);
-        quickSort(array, leftPointer +1 , highIndex);
+        quickSort(array, lowIndex, leftPointer - 1);
+        quickSort(array, leftPointer + 1, highIndex);
+    }
+
+    private static int getPivot(int[] array, int lowIndex, int highIndex) {
+        if (isRandomPivot) {
+            int pivotIndex = new Random().nextInt(highIndex - lowIndex) + lowIndex;
+            int pivot = array[pivotIndex];
+            swap(array, pivotIndex, highIndex);
+            return pivot;
+        } else return array[highIndex];
     }
 
     private static int partition(int[] array, int lowIndex, int highIndex, int pivot) {
         var leftPointer = lowIndex;
         //since our pivot is the last index, our right must be 1 index before
-        var rightPointer = highIndex -1;
+        var rightPointer = highIndex - 1;
 
-        while(leftPointer < rightPointer) {
+        while (leftPointer < rightPointer) {
 
             //move to the left until find a number greater than the pivot, or hit the rightpointer
             while (array[leftPointer] <= pivot && leftPointer < rightPointer)
                 leftPointer++;
 
             //move to the right the until a number less than the pivot, or hit the leftPointer
-            while(array[rightPointer] >= pivot && leftPointer < rightPointer)
+            while (array[rightPointer] >= pivot && leftPointer < rightPointer)
                 rightPointer--;
 
             swap(array, leftPointer, rightPointer);
         }
 
         //Choose the pivot in a random way, it the last value could potentially be out of order
-        if(array[leftPointer] > array[highIndex])
+        if (array[leftPointer] > array[highIndex])
             swap(array, leftPointer, highIndex);
         else
             leftPointer = highIndex;
-//
+
         return leftPointer;
     }
 
     private static void swap(int[] array, int pivotIndex, int highIndex) {
         var temp = array[pivotIndex];
         array[pivotIndex] = array[highIndex];
-        array[highIndex]  = temp;
+        array[highIndex] = temp;
     }
 }
--- src/test/java/br/com/algo/algorithms/sorting/QuickSortTest.java
+++ src/test/java/br/com/algo/algorithms/sorting/QuickSortTest.java
@@ -1,54 +1,121 @@
 package br.com.algo.algorithms.sorting;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class QuickSortTest {
 
     @Test
-    public void dummyTest() {
-        QuickSort.sort(new int[] {12,5,3,4,14,9});
+    @DisplayName("RP - Orderna array com numeros gerados aleatorios")
+    public void generatedTest1() {
+        int[] arr = getRandomNumbers(0, 99999);
+        int[] result = QuickSort.sort(arr.clone(), true);
+        Arrays.sort(arr);
+        assertArrayEquals(arr, result);
+    }
+
+    @Test
+    @DisplayName("RP - Ordena lista com 1 registro")
+    public void test1() {
+        int[] arr = {3};
+        int[] result = QuickSort.sort(arr, true);
+        assertArrayEquals(arr, result);
+    }
+
+    @Test
+    @DisplayName("RP - Ordena lista com varios registros")
+    public void test2() {
+        int[] expected = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] result = QuickSort.sort(new int[]{8, 3, 5, 6, 7, 0, 9, 4, 1, 2}, true);
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("RP - Ordena lista ja ordenada")
+    public void test3() {
+        int[] expected = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] result = QuickSort.sort(expected, true);
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("RP - Ordena lista com todos valores iguais")
+    public void test4() {
+        int[] expected = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int[] result = QuickSort.sort(expected, true);
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("RP - Ordena lista ordenada decrescente")
+    public void test5() {
+        int[] expected = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] result = QuickSort.sort(new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}, true);
+        assertArrayEquals(expected, result);
+    }
+
+    // --------------------------------------------------
+
+    @Test
+    @DisplayName("Orderna array com numeros gerados aleatorios")
+    public void generatedTest() {
+        int[] arr = getRandomNumbers(0, 99999);
+        int[] result = QuickSort.sort(arr.clone(), false);
+        Arrays.sort(arr);
+        assertArrayEquals(arr, result);
+    }
+
+    @NotNull
+    private int[] getRandomNumbers(int from, int to) {
+        int[] arr = new int[999];
+        for (int i = 0; i < 999; i++)
+            arr[i] = new Random().nextInt(Math.abs(from - to));
+        return arr;
     }
 
     @Test
     @DisplayName("Ordena lista com 1 registro")
-    public void test1() {
+    public void test6() {
         int[] arr = {3};
-        int[] result = QuickSort.sort(arr);
+        int[] result = QuickSort.sort(arr, false);
         assertArrayEquals(arr, result);
     }
 
     @Test
     @DisplayName("Ordena lista com varios registros")
-    public void test2() {
-        int[] expected = {0,1,2,3,4,5,6,7,8,9};
-        int[] result = QuickSort.sort(new int[] {8,3,5,6,7,0,9,4,1,2});
+    public void test7() {
+        int[] expected = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] result = QuickSort.sort(new int[]{8, 3, 5, 6, 7, 0, 9, 4, 1, 2}, false);
         assertArrayEquals(expected, result);
     }
 
     @Test
     @DisplayName("Ordena lista ja ordenada")
-    public void test3() {
-        int[] expected = {0,1,2,3,4,5,6,7,8,9};
-        int[] result = QuickSort.sort(expected);
+    public void test8() {
+        int[] expected = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] result = QuickSort.sort(expected, false);
         assertArrayEquals(expected, result);
     }
 
     @Test
     @DisplayName("Ordena lista com todos valores iguais")
-    public void test4() {
-        int[] expected = {0,0,0,0,0,0,0,0,0,0,0,0};
-        int[] result = QuickSort.sort(expected);
+    public void test9() {
+        int[] expected = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int[] result = QuickSort.sort(expected, false);
         assertArrayEquals(expected, result);
     }
 
     @Test
     @DisplayName("Ordena lista ordenada decrescente")
-    public void test5() {
-        int[] expected = {0, 1,2,3,4,5,6,7,8,9};
-        int[] result = QuickSort.sort(new int[] {9,8,7,6,5,4,3,2,1,0});
+    public void test0() {
+        int[] expected = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] result = QuickSort.sort(new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}, false);
         assertArrayEquals(expected, result);
     }
 
